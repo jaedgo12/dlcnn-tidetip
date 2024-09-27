015b98f1131b8b2809058889fcf91506542cdbe4
jaedgo12 <jaygohweiwern@gmail.com>
2024-09-27 10:01:57 +0800

    switch camera input to local camera

--- mysite/cam_app/camera.py
+++ mysite/cam_app/camera.py
@@ -25,7 +25,7 @@ class VideoCamera(object):
         # Using OpenCV to capture from device 0. If you have trouble capturing
         # from a webcam, comment the line below out and use a video file
         # instead.
-        self.video = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+        self.video = cv2.VideoCapture(2, cv2.CAP_DSHOW)
         # If you decide to use video.mp4, you must have this file in the folder
         # as the main.py.
         # self.video = cv2.VideoCapture('video.mp4')
